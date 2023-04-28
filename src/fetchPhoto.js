55c5a746726e91371f3a98977dd62d6fc767f1ec
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-04-28 21:10:22 +0200

    +

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,11 +1,32 @@
-const URL = 'https://pixabay.com/api/'
-const API_KEY = '35828732-80dbae6f7a9b2a665dfdc53c4'
+import axios from "axios";
 
-function fetchPhoto(query) {
-   return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`).then((data) => { return data.json(); })
-    
+export default class ApiService {
+   static ENDPOINT = "https://pixabay.com/api/";
+   static API_KEY = "35828732-80dbae6f7a9b2a665dfdc53c4";
+
+   constructor() {
+      this.query = "";
+      this.page = 1;
+   }
+
+   getfetchPhoto() {
+      const url = `${ApiService.ENDPOINT}?key=${ApiService.API_KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+      // return fetch(url).then((data) => {
+      //    this.incrementPage()
+      //    return data.json();
+      // })
+      return axios.get(url).then((data) => {
+         this.incrementPage();
+         console.log(data);
+         return data.data
+      })
+   }
+
+   incrementPage() { 
+      this.page += 1;
+   }
+
+   resetPage() { 
+     this.page = 1; 
+   }
 }
-
-export { fetchPhoto };
-
-// .then(({ hits }) => { console.log(hits) })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './css/styles.css';
-import axios from 'axios';
-import { fetchPhoto } from './fetchPhoto.js';
+import ApiService from './fetchPhoto.js'
+import LoadMoreBtn from './components/LoadMoreBtn';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 
 const inputText = document.querySelector('input');
 const form = document.querySelector('form');
@@ -9,38 +13,84 @@ const btn = document.querySelector('button');
 const gallery = document.querySelector('.gallery');
 
 
+
 const URL = 'https://pixabay.com/api/'
 const API_KEY = '35828732-80dbae6f7a9b2a665dfdc53c4'
+
+const apiService = new ApiService()
+
+const loadMore = new LoadMoreBtn({
+    selector: "#load-more",
+    isHidden: true,
+})
+console.log(apiService);
+
+loadMore.button.addEventListener("click", fetchMorePhoto);
 
 form.addEventListener("submit", onSubmit)
 
 
 
 function onSubmit(e) {
-    e.preventDefault();
-    clearHTML();
-    const form = e.currentTarget;
-    const query = form.elements.searchQuery.value;
+  e.preventDefault();
 
-    
-     fetchPhoto(query).then(({ hits }) => {
-         if (hits.length === 0) {
-             return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-         } 
-         
-         createMarkup(hits);
-    }
-       
-    ).catch(onError).finally(() => form.reset())
+  const form = e.currentTarget;
+
+  loadMore.show();
+
+  apiService.resetPage();
+  clearHTML();
+
+    apiService.query = form.elements.searchQuery.value;  
+  console.log(apiService);
+   
+ 
+  getPhotoMarkup()
+
+  inputText.value = ""
+    // .catch(onError).finally(() => form.reset())
 }
-         
+    
 
+async function getPhotoMarkup() { 
+  loadMore.disable();
+  return await apiService.getfetchPhoto().then(({ hits, totalHits }) => {
+    //  console.log(hits)
+    if (hits.length === 0) {
+      loadMore.hide()
+      return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
 
+    console.log(totalHits);
+
+    getTotalHits(totalHits)
+    createMarkup(hits)
+    
+    
+  }).catch(onError)
+}
+
+async function fetchMorePhoto() {
+   await getPhotoMarkup();
+}
+
+async function getTotalHits() { 
+  await apiService.getfetchPhoto().then(({ totalHits }) =>
+  {
+     if (totalHits === 0) {
+      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+    } else if (totalHits < 40) {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+    }
+ } )
+}
 
    function createMarkup(hits) {
         const markup = hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
  `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <a class='gallery__link' href='${largeImageURL}'><img class='gallery__image' src="${webformatURL}" alt="${tags}" loading="lazy" width='360' height='260' /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -52,15 +102,17 @@ function onSubmit(e) {
     </p>
     <p class="info-item">
       <b>Comments</b>
-      <span>${likes}</span>
+      <span>${comments}</span>
     </p>
     <p class="info-item">
       <b>Downloads</b>
       <span>${downloads}</span>
     </p>
+    
   </div>
 </div>`).join('');
-        return gallery.innerHTML = markup;
+     loadMore.enable();
+        return gallery.insertAdjacentHTML("beforeend", markup);
         // gallery.innerHTML = markup;
         
     }
@@ -70,5 +122,15 @@ console.log(err)
 }
 
 function clearHTML() {
-    gallery.innerHTML = ""
+  gallery.innerHTML = ""
 }
+
+
+
+// const lightbox = new SimpleLightbox('.gallery a', { 
+   
+//    captionsData: 'alt',
+//    captionPosition: 'bottom',
+//    captionDelay: 250,
+   
+//  });
